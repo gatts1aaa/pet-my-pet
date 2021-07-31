e0e5ea6a933d22fa2176649c9da4489f205b56c0
Marie-Pierre Jeffard <mariep.email@gmail.com>
2021-07-31 14:08:09 +0200

    completion des models booking et user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :own_pets, foreign_key: 'owner_id', class_name: 'Pet'
+  has_many :owner_bookings, through: :own_pets, source: :bookings
+  has_many :customer_bookings, foreign_key: 'customer_id', class_name: 'Booking'
+  has_many :customer_pets, -> { distinct }, through: :customer_bookings, source: :pet
 end
