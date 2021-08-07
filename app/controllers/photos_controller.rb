390b6abab03817d6f77bcd9a6e64ff7962329f8f
Lee LAIB <l.laib69@gmail.com>
2021-08-07 10:35:34 +0200

    cloudinary configuration rev1

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,31 @@
+class PhotosController < ApplicationController
+  before_action :set_user
+  before_action :set_photo, only: [:show, :destroy]
+
+  # GET /users/1/photos
+  def index
+  end
+
+  # GET /users/1/photos/abc
+  def show
+    @downloaded = @photo.download
+  end
+
+  # DELETE /users/1/photos/abc
+  def destroy
+    @photo.purge
+    respond_to do |format|
+      format.html { redirect_to @user, notice: 'Votre photo a été correctement supprimée.' }
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_photo
+    @photo = @user.photos.find(params[:id])
+  end
+end
---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dhamloe8c'
+  config.api_key = '483384788117266'
+  config.api_secret = 'pXGtNYlz7DQylpLhvwYees0Il4w'
+  config.secure = true
+  config.cdn_subdomain = true
+end
