490a5f2250fc31a0527d5f3ba5aee96ca142942d
Lee LAIB <l.laib69@gmail.com>
2021-08-07 12:02:13 +0200

    cloudinary-profile-rev2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @user = User.find(params[:id])
   end
 
   # GET /users/new
@@ -60,11 +61,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'profile correctement supprimé.' }
       format.json { head :no_content }
-  end
-
-  def show
-    @user = User.find(params[:id])
-
+    end
   end
 
   private
@@ -76,6 +73,7 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
     params.require(:user).permit(:name, :description, :avatar, :photos)
+  end
 
   def article_params
     params.require(:user).permit(:name, :description, :photo)
@@ -86,4 +84,3 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 end
-
